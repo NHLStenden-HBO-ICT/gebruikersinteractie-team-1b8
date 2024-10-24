6029103bd8232220efd3859a245308f3029cba5c
Jillert <jillertsijens@gmail.com>
2024-10-24 17:57:19 +0200

    Adding MerchantQuest  Startscreen, fightscreen, defeatscreen, winscreen and mainwindow

---
+++ MerchantQuest/MerchantQuest/fight_win.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MerchantQuest
+{
+    /// <summary>
+    /// Interaction logic for fight_win.xaml
+    /// </summary>
+    public partial class fight_win : Page
+    {
+        public fight_win()
+        {
+            InitializeComponent();
+        }
+
+        private void Continue_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new StartScreen());
+        }
+    }
+}
