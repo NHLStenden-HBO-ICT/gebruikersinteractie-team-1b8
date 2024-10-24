d5709add262f64c0b884b183922dcd3ef8c25047
Joep-Git <joepwestera@gmail.com>
2024-10-24 18:06:55 +0200

    Settings toegevoegd en kaart

---
+++ MerchantQuest/MerchantQuest/Page2.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Merchant_Quest
+{
+    /// <summary>
+    /// Interaction logic for Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void Settingsbutton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Page1());
+        }
+
+        private void WindowMode_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
